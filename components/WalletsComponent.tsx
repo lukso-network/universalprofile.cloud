b2a847a50f68013409a7930f5c9cb75f2a36c5ab
Maxime <maximeviard@hotmail.fr>
2022-08-29 14:06:52 +0300

    Set up walletConnect feature

---
+++ components/WalletsComponent.tsx
@@ -0,0 +1,9 @@
+const WalletsComponent = () => {
+  return (
+    <div>
+      <h1>Wallets</h1>
+    </div>
+  );
+};
+
+export default WalletsComponent;
---
+++ pages/connect-wallet.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <h1 className="text-xl font-bold">Connect to Wallet</h1>
+      <div>
+        <div className="flex flex-col items-center">
+          <img src="" alt="lukso-img" />
+          <div>Universal Profile Ext.</div>
+        </div>
+        <div className="flex flex-col items-center">
+          <img src="" alt="lukso-img" />
+          <div>WalletConnect</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
