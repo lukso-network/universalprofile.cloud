bc2de985130ed612fa0ce206c44744d72545b179
Max <maximeviard@hotmail.fr>
2022-09-14 16:27:37 +0200

    Switch to light theme

--- components/LSP8Card/LSP8Card.tsx
+++ components/LSP8Card/LSP8Card.tsx
@@ -1,3 +1,6 @@
+import identicon from 'ethereum-blockies-base64';
+import { useState } from 'react';
+
 interface Props {
   assetJSON: {
     icon: string;
@@ -13,20 +16,43 @@ interface Props {
 }
 
 const LSP8Card: React.FC<Props> = ({ assetJSON }) => {
+  const [showSendBtn, setShowSendBtn] = useState(false);
+
   return (
-    <div className="border border-darkGray p-3 rounded-lg h-[280px] mt-8">
+    <div
+      className="border hover:scale-105 relative border-gray-300 rounded-xl h-[280px] overflow-hidden mt-8"
+      onMouseEnter={() => setShowSendBtn(true)}
+      onMouseLeave={() => setShowSendBtn(false)}
+    >
       <div className="rounded h-[180px] overflow-hidden mb-2">
         <img
           src={assetJSON.image}
           alt="nft-image"
-          className="object-none rounded w-full h-full bg-cover bg-repeat bg-center"
+          className="object-cover rounded w-full h-full bg-cover bg-repeat bg-center"
         />
       </div>
-      <div className="text-xs font-bold text-gray-600 leading-6">
-        {assetJSON.collectionName}
+      <div className="px-3">
+        <div className="font-ibmpBold text-gray-600 leading-6">
+          {assetJSON.collectionName}
+        </div>
+        {assetJSON.tokenId && (
+          <div className="text-sm leading-6">
+            #{parseInt(assetJSON.tokenId)}
+          </div>
+        )}
+        <div className="absolute bottom-[-30%] right-[-30%] rotate-45">
+          <img
+            src={identicon(assetJSON.collectionAddress)}
+            alt="address identicon"
+          />
+        </div>
       </div>
-      {assetJSON.tokenId && (
-        <div className="text-sm leading-6">#{parseInt(assetJSON.tokenId)}</div>
+      {showSendBtn && (
+        <div className="absolute z-1 top-0 left-0 w-full h-full bg-white bg-opacity-50 flex items-center justify-center">
+          <button className="px-3 py-2 font-bold bg-deepPink rounded text-white">
+            Send
+          </button>
+        </div>
       )}
     </div>
   );
