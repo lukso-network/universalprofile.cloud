d37f9f35ddcc7f216314cb1da61e20b65e6c7bb0
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-07-28 11:35:41 +0200

    Config nuxt plugins, add web components

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+// due to Nuxt SSR we need to check if window is defined
+if (typeof window !== 'undefined') {
+  import('@lukso/web-components')
+}
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <lukso-button>Welcome!</lukso-button>
   </div>
 </template>
