0ea4ad7e1f0b3abf58661e9b943ef5ae648fbcb8
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-07-31 14:52:37 +0200

    Address check cd

--- app.vue
+++ app.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
+import { getProfileAddressFromUrl } from '@/utils/getProfileAddressFromUrl'
+
 // due to Nuxt SSR we need to check if window is defined
 if (typeof window !== 'undefined') {
   import('@lukso/web-components')
 }
 
+// setup translations
 const { setupIntl } = useIntl()
 setupIntl(defaultConfig)
+
+// get profile address from url with additional validation checks
+getProfileAddressFromUrl()
 </script>
 
 <template>
