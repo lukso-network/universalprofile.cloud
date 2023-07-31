81d47be186b26689584ecac2784b5f4544a5d27a
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-07-31 14:54:17 +0200

    Profile card

---
+++ components/profile.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const { address, name, backgroundUrl, profileUrl } = useProfileStore()
+</script>
+
+<template>
+  <div>
+    <lukso-card
+      variant="profile"
+      :background-url="backgroundUrl"
+      :profile-url="profileUrl"
+      :profile-address="address"
+      is-full-width
+    >
+      <div slot="content" class="px-6 pb-6 flex flex-col items-center">
+        <lukso-username v-if="name" :name="name" size="large"></lukso-username>
+        <lukso-username
+          :address="address"
+          :size="name ? 'small' : 'large'"
+          slice-by="40"
+        ></lukso-username>
+      </div>
+    </lukso-card>
+  </div>
+</template>
---
+++ pages/[address]/profile.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <div class="max-w-[835px] py-20 px-4 mx-auto">
+      <Profile />
+    </div>
+  </div>
+</template>
