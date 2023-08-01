4c3daf51b03e737bebda28f27ee51226a078caa9
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-08-01 17:39:16 +0200

    change fixture

--- utils/fetchLSP7Assets.ts
+++ utils/fetchLSP7Assets.ts
@@ -15,27 +15,6 @@ export const fetchLSP7Assets = (/*profileAddress: Address*/) => {
       icon: '',
       address: '0x463306b7D641FDff5D3E30947aC96074b705d4E8',
     },
-    {
-      name: 'LUKSO',
-      symbol: 'LYX',
-      amount: '10.000',
-      icon: '',
-      address: '0x463306b7D641FDff5D3E30947aC96074b705d4E8',
-    },
-    {
-      name: 'LUKSO',
-      symbol: 'LYX',
-      amount: '10.000',
-      icon: '',
-      address: '0x463306b7D641FDff5D3E30947aC96074b705d4E8',
-    },
-    {
-      name: 'LUKSO',
-      symbol: 'LYX',
-      amount: '10.000',
-      icon: '',
-      address: '0x463306b7D641FDff5D3E30947aC96074b705d4E8',
-    },
   ] as Lsp7AssetType[]
 
   return lsp7Assets
