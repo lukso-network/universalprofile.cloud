3522f4133878e8b457e7999b991090ec496e1fe0
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-09-06 15:01:57 +0200

    Install/connect button

--- layouts/landing.vue
+++ layouts/landing.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="min-h-screen grid grid-rows-[max-content,auto,max-content]">
+  <div
+    class="min-h-screen grid grid-rows-[max-content,auto,max-content] bg-gradient-to-b from-[rgba(144,155,209,0.10)] via-[rgba(223,184,226,0.10)] to-[rgba(250,224,227,0.10)]"
+  >
     <Navbar />
     <slot />
     <Footer />
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,26 @@
 <script setup lang="ts">
+import { EXTENSION_STORE_LINKS } from '@/shared/config'
+
 definePageMeta({
   layout: 'landing',
 })
+
+const supportedBrowsers = Object.entries(EXTENSION_STORE_LINKS)
+  .filter(entry => entry[1] !== '')
+  .map(browser => browser[0])
 </script>
 
 <template>
   <div>
     <div
-      class="max-w-[950px] py-20 pt-24 px-4 mx-auto relative grid grid-cols-2 gap-7"
+      class="max-w-[950px] py-20 sm:pt-24 px-4 mx-auto relative grid grid-cols-1 gap-7 h-full sm:items-center sm:grid-cols-2"
     >
-      <div>
-        <img src="/images/wallet.png" alt="" class="max-w-[484px]" />
+      <div class="hidden sm:block">
+        <img
+          src="/images/wallet.png"
+          alt=""
+          class="max-w-full lg:max-w-[484px]"
+        />
       </div>
       <div>
         <lukso-card is-full-width>
@@ -28,8 +38,21 @@ definePageMeta({
                 :html-content="$formatMessage('landing_hero_description')"
               ></lukso-sanitize>
             </div>
+            <ConnectOrInstallButton />
           </div>
         </lukso-card>
+        <div
+          class="flex pt-6 gap-2 justify-center items-center paragraph-inter-12-regular"
+        >
+          <span>{{ $formatMessage('landing_hero_supported_browsers') }}</span>
+          <lukso-icon
+            v-for="browser in supportedBrowsers"
+            :key="browser"
+            :name="`logo-${browser}`"
+            color="neutral-20"
+            secondary-color="neutral-100"
+          />
+        </div>
       </div>
     </div>
   </div>
