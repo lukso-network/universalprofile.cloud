93e3812e2aaefa8c1f29a9e576ef8cec634f5004
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-09-12 14:16:00 +0200

    Add amount field validations

--- components/send-draft.vue
+++ components/send-draft.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { fromWei, isAddress } from 'web3-utils'
 import { storeToRefs } from 'pinia'
+import BigNumber from 'bignumber.js'
 
 const { profile: connectedProfile } = useConnectionStore()
 const { asset, receiverAddress, receiver, receiverError, amount, onSend } =
@@ -31,8 +32,56 @@ const handleReceiverChange = async (event: CustomEvent) => {
   }
 }
 
-const handleAmountChange = (event: CustomEvent) => {
-  amount.value = event.detail.value
+const handleKeyDown = (customEvent: CustomEvent) => {
+  const numberRegex = /^[0-9]*\.?[0-9]*$/
+  const event = customEvent.detail.event
+  const input = event.target as HTMLInputElement
+  const key = event.key
+  const allowedKeys = ['Backspace', 'ArrowLeft', 'ArrowRight']
+  const realValueBN = new BigNumber(`${input.value}${key}`)
+  const assetBalanceBN = new BigNumber(`${fromWei(asset.value?.amount || '0')}`)
+  const maxDecimalPlaces = 6
+
+  // check for allowed keys
+  if (allowedKeys.includes(key)) {
+    return
+  }
+
+  // allow only numbers
+  if (!numberRegex.test(key)) {
+    event.preventDefault()
+  } else {
+    // Did it like this otherwise, if a user presses Esc or any non digit key, it would reset the error message
+    receiverError.value = ''
+  }
+
+  // when value is more then balance we set to max value
+  if (realValueBN.gt(assetBalanceBN)) {
+    input.value = fromWei(asset.value?.amount?.toString() || '0')
+    event.preventDefault()
+  }
+
+  // allow only one dot in the value, but not as first character
+  if (key === '.' && (input.value.includes('.') || input.value === '')) {
+    event.preventDefault()
+  }
+
+  // check for max decimal places
+  if (input.value.toString().split('.')[1]?.length >= maxDecimalPlaces) {
+    event.preventDefault()
+  }
+}
+
+const handleKeyUp = (event: CustomEvent) => {
+  const input = event.detail.event.target
+  amount.value = input.value
+}
+
+const handleUnitClick = (event: CustomEvent) => {
+  const input = event.detail.input
+  const total = fromWei(asset.value?.amount?.toString() || '0')
+  input.value = total
+  amount.value = total
 }
 
 const handleSend = () => {
@@ -48,7 +97,7 @@ const handleSend = () => {
     :profile-address="connectedProfile.address"
     is-full-width
   >
-    <div slot="content" class="p-6">
+    <div slot="content" class="p-6 pt-0">
       <div
         class="border border-neutral-90 rounded-12 flex justify-center items-center"
       >
@@ -81,7 +130,9 @@ const handleSend = () => {
               "
               borderless
               is-full-width
-              @on-input="handleAmountChange"
+              @on-key-down="handleKeyDown"
+              @on-key-up="handleKeyUp"
+              @on-unit-click="handleUnitClick"
             ></lukso-input>
           </div>
         </div>
