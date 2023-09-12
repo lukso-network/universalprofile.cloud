19de5f7aa08647f7fe57352c52044256286b922e
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-09-12 09:34:11 +0200

    chore: Remove `profile` namespace from routes (#44)
    Co-authored-by: Hugo Masclet <hugo@lukso.io>

--- shared/routes.ts
+++ shared/routes.ts
@@ -1,17 +1,15 @@
 export const tokenRoute = (profileAddress: Address, tokenAddress: Address) =>
-  `/profile/${profileAddress}/token/${tokenAddress}`
+  `/${profileAddress}/token/${tokenAddress}`
 
 export const nftRoute = (
   profileAddress: Address,
   nftAddress: Address,
   tokenId: string
-) => `/profile/${profileAddress}/nft/${nftAddress}/tokenId/${tokenId}`
+) => `/${profileAddress}/nft/${nftAddress}/tokenId/${tokenId}`
 
-export const profileRoute = (profileAddress: Address) =>
-  `/profile/${profileAddress}`
+export const profileRoute = (profileAddress: Address) => `/${profileAddress}`
 
-export const sendRoute = (profileAddress: Address) =>
-  `/profile/${profileAddress}/send`
+export const sendRoute = (profileAddress: Address) => `/${profileAddress}/send`
 
 export const homeRoute = () => '/'
 
