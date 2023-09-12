a7a2609287b0201e2a6f60a147cf9f3aea738bb6
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-09-12 10:37:33 +0200

    New profile hero

--- components/profile.vue
+++ components/profile.vue
@@ -5,23 +5,31 @@ const { profile } = useProfileStore()
 <template>
   <div class="relative">
     <lukso-card
-      variant="profile"
+      variant="hero"
       :background-url="profile.backgroundImageUrl"
-      :profile-url="profile.profileImageUrl"
-      :profile-address="profile.address"
       is-full-width
       custom-class="rounded-24 shadow-neutral-drop-shadow"
     >
-      <div slot="content" class="px-6 pb-6 flex flex-col items-center">
+      <div slot="content" class="flex flex-col items-center">
+        <lukso-profile
+          :profile-url="profile.profileImageUrl"
+          :profile-address="profile.address"
+          class="mb-4"
+        >
+        </lukso-profile>
         <lukso-username
           v-if="profile.name"
           :name="profile.name"
           size="large"
+          address-color="neutral-100"
+          name-color="neutral-100"
         ></lukso-username>
         <lukso-username
           :address="profile.address"
           :size="profile.name ? 'small' : 'large'"
           slice-by="40"
+          address-color="neutral-100"
+          name-color="neutral-100"
         ></lukso-username>
       </div>
     </lukso-card>
