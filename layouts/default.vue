5c5f8e2799cfc0b1ec8b64a5595d78f4ace85a13
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-09-18 09:39:14 +0200

    Fix test

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
+import { isAddress } from 'web3-utils'
+
 onMounted(async () => {
   try {
     const profileAddress = useRouter().currentRoute.value.params?.profileAddress
+
+    if (!isAddress(profileAddress)) {
+      navigateTo(notFoundRoute())
+    }
+
     await setupViewedProfile(profileAddress)
     await setupViewedAssets(profileAddress)
   } catch (error) {
