d2578b1b7fd1b2f2f8fce8de8a137ee419934643
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-10-03 11:17:09 +0200

    Update footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,41 +1,47 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Provider } from '@lukso/web-components/dist/components/lukso-share'
+
+const providers: Provider = ['twitter', 'discord', 'github']
+</script>
 
 <template>
-  <lukso-footer>
+  <lukso-footer :providers="providers">
     <div
-      slot="top"
-      class="grid gap-6 items-center sm:gap-10 sm:grid-cols-[repeat(2,max-content)]"
+      slot="links"
+      class="grid gap-4 items-center grid-cols-1 grid-rows-2 xl:grid-cols-[repeat(3,max-content)] xl:gap-10 xl:grid-rows-1"
     >
-      <a
-        href="https://lukso.network/faq"
-        class="nav-apax-12-medium-uppercase text-purple-41 hover:underline hover:text-purple-31"
-        target="_blank"
-      >
-        {{ $formatMessage('footer_faq_text') }}
-      </a>
-      <a
-        :href="$formatMessage('footer_need_help_url')"
-        class="nav-apax-12-medium-uppercase text-purple-41 hover:underline hover:text-purple-31"
-      >
-        {{ $formatMessage('footer_need_help_text') }}
-      </a>
-    </div>
-
-    <div slot="bottom" class="gap-8 flex items-center">
-      <a
-        href="https://new-creative-economies.foundation/privacy"
-        class="paragraph-inter-12-medium hover:underline"
-        target="_blank"
-      >
-        {{ $formatMessage('footer_privacy_policy_text') }}
-      </a>
-      <a
-        href="https://lukso.network/terms"
-        class="paragraph-inter-12-medium hover:underline cursor-pointer"
-        target="_blank"
-      >
-        {{ $formatMessage('footer_terms_text') }}
-      </a>
+      <div class="flex gap-10">
+        <a
+          href="https://lukso.network/faq"
+          class="nav-apax-12-medium-uppercase text-purple-41 hover:underline hover:text-purple-31"
+          target="_blank"
+        >
+          {{ $formatMessage('footer_faq_text') }}
+        </a>
+        <a
+          :href="$formatMessage('footer_need_help_url')"
+          class="nav-apax-12-medium-uppercase text-purple-41 hover:underline hover:text-purple-31"
+        >
+          {{ $formatMessage('footer_need_help_text') }}
+        </a>
+      </div>
+      <div class="hidden w-0.5 h-4 bg-purple-63 xl:block"></div>
+      <div class="flex gap-10">
+        <a
+          href="https://new-creative-economies.foundation/privacy"
+          class="nav-apax-12-medium-uppercase text-purple-41/70 hover:underline hover:text-purple-31/70"
+          target="_blank"
+        >
+          {{ $formatMessage('footer_privacy_policy_text') }}
+        </a>
+        <a
+          href="https://lukso.network/terms"
+          class="nav-apax-12-medium-uppercase text-purple-41/70 hover:underline hover:text-purple-31/70"
+          target="_blank"
+        >
+          {{ $formatMessage('footer_terms_text') }}
+        </a>
+      </div>
     </div>
   </lukso-footer>
 </template>
