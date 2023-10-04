7c0bada30cd3b37ff2de200ff0d676d95040873f
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-10-04 16:51:40 +0200

    Fix images grid

--- components/AssetImages.vue
+++ components/AssetImages.vue
@@ -13,13 +13,15 @@ defineProps<Props>()
     <div class="heading-inter-14-bold pb-3">
       {{ $formatMessage('token_details_images') }}
     </div>
-    <div
-      v-for="(image, index) in images"
-      :key="index"
-      class="w-14 h-14 rounded-8 bg-cover bg-position-center bg-neutral-90"
-      :style="{
-        backgroundImage: `url(${formatUrl(image[0]?.url)})`,
-      }"
-    ></div>
+    <div class="flex gap-4 flex-wrap">
+      <div
+        v-for="(image, index) in images"
+        :key="index"
+        class="w-14 h-14 rounded-8 bg-cover bg-position-center bg-neutral-90"
+        :style="{
+          backgroundImage: `url(${formatUrl(image[0]?.url)})`,
+        }"
+      ></div>
+    </div>
   </div>
 </template>
