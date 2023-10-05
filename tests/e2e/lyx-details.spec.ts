ab1de634a25762dcd1718d333b1a0ca0e23e0161
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-10-05 14:54:42 +0200

    fix: Redirect to `my.universalprofile.cloud` when clicking on logo (#103)

--- tests/e2e/lyx-details.spec.ts
+++ tests/e2e/lyx-details.spec.ts
@@ -4,9 +4,9 @@ import { test } from './helpers/fixtures'
 
 test('user can see lyx details page when not connected', async ({ page }) => {
   await page.goto('/0x64DE43F67e533b59A5791E6aB1e5a80626E10710/lyx-details')
+  await page.waitForLoadState('networkidle')
+  await expect(page).toHaveScreenshot('lyx-details-disconnected.png')
   await expect(page).toHaveURL(
     '/0x64DE43F67e533b59A5791E6aB1e5a80626E10710/lyx-details'
   )
-  await page.waitForLoadState('networkidle')
-  await expect(page).toHaveScreenshot('lyx-details-disconnected.png')
 })
