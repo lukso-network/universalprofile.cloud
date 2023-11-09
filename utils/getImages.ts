289bd4d4880d19cdef4c685bfdbe113ac2f9be2b
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-11-09 11:04:09 +0100

    Fix missing images

--- utils/getImages.ts
+++ utils/getImages.ts
@@ -92,6 +92,7 @@ export const getAndConvertImage = async (
       ...optimalImage,
       base64: formatUrl(optimalImage.url),
       // base64: await fetchAndConvertImage(optimalImage.url), // TODO add base when cache storage is added
+      id: getImageId(optimalImage),
     } as ImageMetadataEncoded
   }
 }
