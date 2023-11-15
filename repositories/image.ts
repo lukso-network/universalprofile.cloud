b1a5efcdf78fde2841f9d5014e175ba992972a19
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-11-15 16:16:17 +0100

    fix: missing images (#159)
    Co-authored-by: Andreas Richter <708186+richtera@users.noreply.github.com>

--- repositories/image.ts
+++ repositories/image.ts
@@ -6,29 +6,21 @@ export class ImageRepository extends Repository<ImageModel> {
   use = ImageModel
 
   getImage(id?: string) {
-    const { selectedChainId } = storeToRefs(useAppStore())
-
     if (!id) {
       return
     }
 
-    const image = this.repo(ImageModel)
-      .where('chainId', selectedChainId.value)
-      .find(id)
+    const image = this.repo(ImageModel).find(id)
 
     return image
   }
 
   getImages(ids?: string[]) {
-    const { selectedChainId } = storeToRefs(useAppStore())
-
     if (!ids?.length) {
       return []
     }
 
-    const images = ids.map(id =>
-      this.repo(ImageModel).where('chainId', selectedChainId.value).find(id)
-    )
+    const images = ids.map(id => this.getImage(id))
 
     return images
   }
