34ad5fe5891fbd7202d83c955b0bf2a482be19ae
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-11-30 10:17:30 +0100

    Pass network with params

--- utils/externalUrls.ts
+++ utils/externalUrls.ts
@@ -3,3 +3,9 @@ export const explorerTransactionUrl = (transactionHash: string) => {
 
   return `${currentNetwork.value.explorerUrl}/tx/${transactionHash}`
 }
+
+export const discoveryDappUrl = () => {
+  const { currentNetwork } = storeToRefs(useAppStore())
+
+  return `${BASE_UP_CLOUD_URL}?network=${currentNetwork.value.id}`
+}
