b45fa49de26a7b2746f20053dbc9567dce932aef
Dominik Zborowski <dominik.zborowski@gmail.com>
2024-01-03 16:32:47 +0100

    Fix import

--- types/device.ts
+++ types/device.ts
@@ -1,22 +1,4 @@
-export type Device = {
-  userAgent: string
-  isDesktop: boolean
-  isIos: boolean
-  isAndroid: boolean
-  isMobile: boolean
-  isMobileOrTablet: boolean
-  isDesktopOrTablet: boolean
-  isTablet: boolean
-  isWindows: boolean
-  isMacOS: boolean
-  isApple: boolean
-  isSafari: boolean
-  isFirefox: boolean
-  isEdge: boolean
-  isChrome: boolean
-  isSamsung: boolean
-  isCrawler: boolean
-}
+import type { Device } from '@nuxtjs/device/dist/runtime/types'
 
 export type NavigatorExtended = Navigator & {
   brave?: { isBrave: () => Promise<boolean> }
