6f532f8ea978d3fbac2a34a8111bca261e07911a
Dominik Zborowski <dominik.zborowski@gmail.com>
2024-01-16 14:17:01 +0100

    Refactors

--- pages/asset/[nftAddress]/tokenId/[tokenId].vue
+++ pages/asset/[nftAddress]/tokenId/[tokenId].vue
@@ -24,6 +24,7 @@ const isOwned = computed(() => {
     isConnected &&
     connectedProfile &&
     asset.value?.address &&
+    asset.value?.balance &&
     connectedProfile.value?.receivedAssetAddresses?.includes(
       asset.value?.address
     )
