e41d49ef63fc1e18718ef4f5a8031616d7739ace
Dominik Zborowski <dominik.zborowski@gmail.com>
2024-02-03 22:06:11 +0100

    PR remarks

--- utils/paddedToAddress.ts
+++ utils/paddedToAddress.ts
@@ -1,15 +1,23 @@
+import web3 from 'web3'
+
 /**
  * Remove padding from address
  *
  * @param address
  * @returns
  */
-export const paddedToAddress = (address: string) => {
-  if (address.slice(0, 2) === '0x') {
+export const paddedToAddress = (address?: string) => {
+  if (!address || address.length < 42) {
+    return ''
+  }
+
+  // remove 0x
+  if (address.startsWith('0x')) {
     address = address.slice(2)
   }
 
-  while (address.slice(0, 2) === '00') {
+  // remove padding
+  while (address.slice(0, 2) === '00' && !web3.utils.isAddress(address)) {
     address = address.slice(2)
   }
 
