bdcde5965a130a73ffe6940245bc6daad12bbf4f
Dominik Zborowski <dominik.zborowski@gmail.com>
2024-02-08 09:15:18 +0100

    Comments

--- utils/assetChecks.ts
+++ utils/assetChecks.ts
@@ -1,16 +1,51 @@
 import { ASSET_TYPES } from '@/shared/enums'
 
+/**
+ * Check if passed asset is LYX token
+ * @param asset
+ * @returns
+ */
 export const isLyx = (asset?: Asset) => !!asset?.isNativeToken
 
+/**
+ * Check if passed asset is collectible
+ *
+ * @param asset
+ * @returns
+ */
 export const isCollectible = (asset?: Asset) =>
   asset?.tokenType === 'NFT' || asset?.tokenType === 'COLLECTION'
 
+/**
+ * Check if passed asset is token
+ *
+ * @param asset
+ * @returns
+ */
 export const isToken = (asset?: Asset) =>
   asset?.tokenType === 'TOKEN' || isLyx(asset)
 
+/**
+ * Check if passed asset is LSP7 token
+ *
+ * @param asset
+ * @returns
+ */
 export const isLsp7 = (asset?: Asset) => asset?.standard === ASSET_TYPES.LSP7
 
+/**
+ * Check if passed asset is LSP8 token
+ *
+ * @param asset
+ * @returns
+ */
 export const isLsp8 = (asset?: Asset) => asset?.standard === ASSET_TYPES.LSP8
 
+/**
+ * Check if passed asset has a token id
+ *
+ * @param asset
+ * @returns
+ */
 export const hasTokenId = (asset?: Asset) =>
   !!asset?.tokenId?.length && asset?.tokenId !== '0x'
