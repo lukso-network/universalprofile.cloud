fff799b547cd989c8197948508a6c0ca629cd5eb
Dominik Zborowski <dominik.zborowski@gmail.com>
2024-02-16 18:37:13 +0100

    Mobile adjustments

--- components/AppPageLoader.vue
+++ components/AppPageLoader.vue
@@ -18,7 +18,7 @@ const isLoaded = computed(
 <template>
   <div class="relative">
     <div
-      class="relative px-4 py-20"
+      class="relative px-4 py-6 sm:py-20"
       :class="{
         'opacity-0': !isLoaded,
 
