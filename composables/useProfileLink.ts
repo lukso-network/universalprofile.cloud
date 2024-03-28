5467ccc2b27e2f35c0cdc2750289abf0f74a43d3
Andreas Richter <708186+richtera@users.noreply.github.com>
2024-03-28 09:33:30 -0400

    fix: Add resolved and link into Profile.

---
+++ composables/useProfileLink.ts
@@ -0,0 +1,35 @@
+import { toChecksumAddress } from 'web3-utils'
+import { isAddress } from 'web3-validator'
+
+export async function resolveProfile(_address: string): Promise<{
+  resolved: string
+  link: string
+  address: Address
+  checksummed: Address
+}> {
+  try {
+    const { resolved, link, address, checksummed } = (await fetcher({
+      url: `https://m.profile.link/${_address}/resolve`,
+      method: 'GET',
+    })) as {
+      resolved: string
+      link: string
+      address: Address
+      checksummed: Address
+    }
+    return { address, checksummed, resolved, link }
+  } catch {
+    if (!isAddress(_address)) throw new Error('Invalid address')
+    const checksummed = toChecksumAddress(_address) as Address
+    return {
+      address: _address as Address,
+      checksummed: checksummed,
+      resolved: _address as string,
+      link: `https://m.profile.link/${checksummed}`,
+    }
+  }
+}
+
+export function useProfileLink(_address: Address) {
+  return useAsyncData(() => resolveProfile(_address))
+}
