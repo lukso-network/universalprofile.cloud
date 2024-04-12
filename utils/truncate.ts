f3a91d4fe3759a77c65777ffe984162890f21f8f
Dominik Zborowski <dominik.zborowski@gmail.com>
2024-04-12 13:24:38 +0200

    Comment

--- utils/truncate.ts
+++ utils/truncate.ts
@@ -1,3 +1,10 @@
+/**
+ * Truncate string to given length and add '...' at the end
+ *
+ * @param value
+ * @param length
+ * @returns
+ */
 export const truncate = (value: string, length: number) => {
   if (length <= 0 || !value) {
     return ''
