fcabf963842f423bb6620d6c2625bcc868f621a4
Dominik Zborowski <dominik.zborowski@gmail.com>
2024-07-09 11:14:40 +0200

    feat: show 3D files in images section (#363)

---
+++ components/AssetAssetsWrapper.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+type Props = {
+  assets?: AssetMetadata[]
+  fileTypes?: AssetFileType[]
+}
+
+const props = defineProps<Props>()
+
+const filteredAssets = computed(() => {
+  return props.assets?.filter(asset => {
+    const assetType = getAssetType(asset)
+    return props.fileTypes?.includes(assetType)
+  })
+})
+</script>
+
+<template>
+  <slot :filtered-assets="filteredAssets" />
+</template>
