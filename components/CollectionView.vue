2e342202412b4fe12941dba26cae7f24713c592f
Dominik Zborowski <dominik.zborowski@gmail.com>
2024-07-25 13:36:40 +0200

    Adjust comments

--- components/CollectionView.vue
+++ components/CollectionView.vue
@@ -182,7 +182,7 @@ onMounted(async () => {
           <AppPlaceholderLine class="h-[28px] w-[100px] rounded-8" />
         </div>
 
-        <!-- Attributes dropdowns -->
+        <!-- Attributes filter -->
         <lukso-select
           v-for="attribute in attributesData?.attributes"
           :key="attribute.id"
@@ -208,7 +208,7 @@ onMounted(async () => {
           @on-select="handleSelectAttribute"
         ></lukso-select>
 
-        <!-- TokenId Search -->
+        <!-- Search filter -->
         <lukso-search
           size="small"
           hide-loading
@@ -230,8 +230,9 @@ onMounted(async () => {
       ></lukso-select>
     </div>
 
-    <!-- Selected attributes -->
+    <!-- Selected filters -->
     <div v-if="hasFiltersSelected" class="flex flex-wrap gap-2 pb-4">
+      <!-- Selected attributes -->
       <lukso-tag
         v-for="attribute in selectedAttributes"
         :key="attribute.id"
