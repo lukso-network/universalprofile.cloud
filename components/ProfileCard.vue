906ff798fcbdb7c9e3aa32d2bfc87f32eebac275
Dominik Zborowski <dominik.zborowski@gmail.com>
2024-07-31 11:24:25 +0200

    feat: new profile card (#375)

--- components/ProfileCard.vue
+++ components/ProfileCard.vue
@@ -2,6 +2,7 @@
 const profile = useProfile().viewedProfile()
 const { isMobile } = useDevice()
 const { showModal } = useModal()
+const { formatMessage } = useIntl()
 
 const profileBackground = useProfileBackground(profile, 880)
 const profileAvatar = useProfileAvatar(profile, 96)
@@ -21,65 +22,133 @@ const handlePreviewProfileImage = () => {
     size: 'auto',
   })
 }
+
+const hasDescription = computed(
+  () => profile?.value?.description && profile.value.description !== ''
+)
+
+const hasTags = computed(
+  () => profile?.value?.tags && profile.value.tags?.length > 0
+)
 </script>
 
 <template>
   <div class="relative">
     <lukso-card
-      variant="hero"
+      variant="dapp"
       :background-url="profileBackground?.url"
       is-full-width
       shadow="small"
       :border-radius="isMobile ? 'none' : 'medium'"
-      class="-mx-4 -mt-6 mb-22 w-screen sm:mx-0 sm:mt-0 sm:w-full"
+      class="-mx-4 -mt-6 w-screen sm:mx-0 sm:mt-0 sm:w-full"
     >
-      <div slot="content" class="relative flex size-full flex-col items-center">
+      <div slot="header" class="relative flex size-full flex-col items-center">
         <ProfileCardShare :profile="profile" />
-        <div class="relative bottom-[-195px] text-center">
-          <div class="flex cursor-pointer flex-col items-center">
-            <lukso-profile
-              :profile-url="profileAvatar?.url"
-              :profile-address="profile?.address"
-              size="x-large"
-              has-identicon
-              class="relative z-[1] inline-flex rounded-full outline outline-4 outline-neutral-100 transition hover:scale-[1.02]"
-              @click="handlePreviewProfileImage"
-            >
-            </lukso-profile>
-            <lukso-tooltip
-              variant="light"
-              offset="110"
-              is-clipboard-copy
-              :copy-text="$formatMessage('profile_card_copy_address')"
-              :copy-value="profile?.address"
-            >
-              <div
-                class="paragraph-ptmono-16-regular relative -top-10 h-0 text-14 opacity-10 transition hover:opacity-30 md:text-24"
-              >
-                {{ profile?.address }}
-              </div>
-            </lukso-tooltip>
-          </div>
-          <lukso-username
-            v-if="profile?.name"
-            :name="profile?.name.toLowerCase()"
-            :address="profile?.address"
-            address-color="neutral-80"
-            size="large"
-            max-width="350"
-            class="mt-4"
-          ></lukso-username>
-          <lukso-username
-            v-else
-            :name="$formatMessage('profile_default_name')"
-            :address="profile?.address"
-            address-color="neutral-80"
-            size="large"
-            max-width="350"
-            class="mt-4"
-            hide-prefix
-          ></lukso-username>
+      </div>
+      <div slot="content" class="relative flex flex-col p-4 pb-6 sm:p-8">
+        <!-- Profile picture -->
+        <div class="absolute top-[-60px]">
+          <lukso-profile
+            :profile-url="profileAvatar?.url"
+            :profile-address="profile?.address"
+            size="2x-large"
+            has-identicon
+            class="relative z-[1] cursor-pointer rounded-full outline outline-4 outline-neutral-100 transition hover:scale-[1.02]"
+            @click="handlePreviewProfileImage"
+          >
+          </lukso-profile>
         </div>
+
+        <!-- Follow Button -->
+        <div class="flex min-h-7 justify-end"></div>
+
+        <div class="mt-8 flex gap-4">
+          <!-- Profile Name -->
+          <lukso-tooltip
+            variant="light"
+            offset="-10"
+            is-clipboard-copy
+            :copy-text="formatMessage('profile_card_copy_address')"
+            :copy-value="profile?.address"
+          >
+            <lukso-username
+              v-if="profile?.name"
+              :name="profile?.name.toLowerCase()"
+              :address="profile?.address"
+              address-color="neutral-80"
+              size="x-large"
+              max-width="350"
+            ></lukso-username>
+            <lukso-username
+              v-else
+              :name="formatMessage('profile_default_name')"
+              :address="profile?.address"
+              address-color="neutral-80"
+              size="x-large"
+              max-width="350"
+              hide-prefix
+            ></lukso-username>
+          </lukso-tooltip>
+
+          <!-- Followers -->
+        </div>
+
+        <!-- Description -->
+        <div
+          v-if="hasDescription"
+          class="paragraph-inter-14-regular mt-4 whitespace-pre-line break-word"
+        >
+          {{ profile?.description }}
+        </div>
+
+        <!-- Tags -->
+        <ul v-if="hasTags" class="mt-4 flex flex-wrap gap-x-4 gap-y-2">
+          <li
+            v-for="(tag, index) in profile?.tags"
+            :key="index"
+            class="inline-flex"
+          >
+            <lukso-tag is-rounded>{{ tag }}</lukso-tag>
+          </li>
+        </ul>
+
+        <!-- Links -->
+        <AppLinks v-if="profile?.links?.length" :links="profile?.links">
+          <template #default="{ socialMediaLinks, otherLinks }">
+            <div class="flex flex-col gap-0 sm:flex-row sm:gap-4">
+              <ul
+                v-if="socialMediaLinks"
+                class="mt-4 flex flex-wrap gap-2 sm:gap-x-4"
+              >
+                <li
+                  v-for="(link, index) in socialMediaLinks"
+                  :key="index"
+                  class="inline-flex"
+                >
+                  <LinkButton
+                    :link="link"
+                    :size="isMobile ? 'medium' : 'small'"
+                  />
+                </li>
+              </ul>
+              <ul
+                v-if="otherLinks"
+                class="mt-2 flex flex-col flex-wrap gap-x-4 gap-y-2 sm:mt-4 sm:flex-row"
+              >
+                <li
+                  v-for="(link, index) in otherLinks"
+                  :key="index"
+                  class="inline-flex"
+                >
+                  <LinkButton
+                    :link="link"
+                    :size="isMobile ? 'medium' : 'small'"
+                  />
+                </li>
+              </ul>
+            </div>
+          </template>
+        </AppLinks>
       </div>
     </lukso-card>
   </div>
