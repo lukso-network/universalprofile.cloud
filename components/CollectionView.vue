e2933eb4f61ee559ed9a1c20582a5489be4df390
Dominik Zborowski <dominik.zborowski@gmail.com>
2024-08-23 16:41:59 +0200

    Fix icon

--- components/CollectionView.vue
+++ components/CollectionView.vue
@@ -261,12 +261,12 @@ onMounted(async () => {
       {{ formatMessage('collection_no_results') }}
     </div>
 
-    <!-- RPC warning -->
+    <!-- RPC info -->
     <div
       v-if="isRpc"
       class="paragraph-inter-12-regular flex items-center gap-3 rounded-8 bg-sky-85 p-4"
     >
-      <lukso-icon name="warning-round"></lukso-icon>
+      <lukso-icon name="information"></lukso-icon>
       <div>
         {{ formatMessage('collection_not_avail_front') }}
         <span
