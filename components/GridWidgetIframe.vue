41eaa67617fb17d882b989876501d9778693e6bb
Eduardo Sacco <saccoeduardo@gmail.com>
2024-09-05 20:24:55 -0300

    use dynamic components instead of v-ifs

---
+++ components/GridWidgetIframe.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+type Props = {
+  src: string
+  title: string
+  allow: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <iframe
+    :src="src"
+    :title="title"
+    :allow="allow"
+    class="overflow-hidden rounded-[10px]"
+    width="100%"
+    height="100%"
+    frameborder="0"
+  ></iframe>
+</template>
