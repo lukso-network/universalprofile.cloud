1d1e47d7823447fc7a0763595a48d0bc848f7868
Eduardo Sacco <saccoeduardo@gmail.com>
2024-09-06 11:04:58 -0300

    Add iframe warning

--- components/GridWidgetIframe.vue
+++ components/GridWidgetIframe.vue
@@ -6,9 +6,30 @@ type Props = {
 }
 
 defineProps<Props>()
+
+const { formatMessage } = useIntl()
 </script>
 
 <template>
+  <!-- TODO: Correct design and copy of the Iframe warning once design is finished -->
+  <div class="absolute right-[10px] top-[10px] z-10">
+    <lukso-tooltip
+      variant="light"
+      size="medium"
+      placement="auto"
+      trigger="mouseenter"
+      :text="formatMessage('widget_iframe_warning')"
+      max-width="300"
+      hide-on-click="false"
+      offset="10"
+      show=""
+      show-arrow=""
+    >
+      <lukso-tag size="x-small" background-color="yellow-65">
+        <lukso-icon name="warning-round" size="small"></lukso-icon>
+      </lukso-tag>
+    </lukso-tooltip>
+  </div>
   <iframe
     :src="src"
     :title="title"
