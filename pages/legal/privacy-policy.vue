8af7be89dbe7ea5a4ca984307024ed5b233934ce
Dominik Zborowski <dominik.zborowski@gmail.com>
2024-09-07 09:44:23 +0200

    Privacy policy

---
+++ pages/legal/privacy-policy.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const { formatMessage } = useIntl()
+</script>
+
+<template>
+  <AppPageLoader>
+    <div class="mx-auto max-w-content">
+      <h1 class="heading-inter-26-semi-bold mb-8">
+        {{ formatMessage('pages_privacy_policy_title') }}
+      </h1>
+      <lukso-sanitize
+        :html-content="formatMessage('pages_privacy_policy_content')"
+      ></lukso-sanitize>
+    </div>
+  </AppPageLoader>
+</template>
