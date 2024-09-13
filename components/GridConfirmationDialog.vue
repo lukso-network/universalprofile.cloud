8277fdc758142ec05213aca9e9cead8c66726223
Dominik Zborowski <dominik.zborowski@gmail.com>
2024-09-13 20:11:40 +0200

    Refactor

--- components/GridConfirmationDialog.vue
+++ components/GridConfirmationDialog.vue
@@ -5,6 +5,7 @@ type Emits = {
 }
 
 const { hasUnsavedGrid } = storeToRefs(useAppStore())
+const { formatMessage } = useIntl()
 const emits = defineEmits<Emits>()
 </script>
 
@@ -13,10 +14,10 @@ const emits = defineEmits<Emits>()
     v-if="hasUnsavedGrid"
     class="paragraph-inter-12-regular fixed bottom-5 left-[calc(50%-250px)] flex w-[500px] items-center justify-between gap-4 rounded-8 border-[3px] border-green-54 bg-neutral-100 p-4"
   >
-    {{ $formatMessage('grid_unsaved_changes_text') }}
+    {{ formatMessage('grid_unsaved_changes_text') }}
     <div class="flex items-center">
       <lukso-button size="small" variant="text" @click="emits('cancel')">{{
-        $formatMessage('grid_unsaved_changes_reset')
+        formatMessage('grid_unsaved_changes_reset')
       }}</lukso-button>
       <!-- TODO create this button variant -->
       <lukso-button
@@ -24,7 +25,7 @@ const emits = defineEmits<Emits>()
         variant="primary"
         custom-class="border-green-54 bg-green-54 hover:bg-green-54 hover:border-green-54"
         @click="emits('save')"
-        >{{ $formatMessage('grid_unsaved_changes_save') }}</lukso-button
+        >{{ formatMessage('grid_unsaved_changes_save') }}</lukso-button
       >
     </div>
   </div>
