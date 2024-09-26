67f6c67be2259dd32984380739466d5e434eb90d
Dominik Zborowski <dominik.zborowski@gmail.com>
2024-09-26 17:13:47 +0200

    Parser updates

--- domains/grid/utils/__tests__/gridParser.spec.ts
+++ domains/grid/utils/__tests__/gridParser.spec.ts
@@ -1,6 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import { GRID_WIDGET_SUPPORTED_PLATFORMS } from '../../shared/config'
-import { parseSupportedPlatformInput } from '../gridWidget'
+import { parseSupportedPlatformInput } from '../gridParser'
 
 const YOUTUBE_IFRAME_ALLOW =
   'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
@@ -13,51 +12,55 @@ describe('Widget Input Parsing', () => {
   it.each([
     [
       'X (Twitter) Post URL',
-      GRID_WIDGET_SUPPORTED_PLATFORMS.X,
-      'https://x.com/someuser/status/123456789',
+      GRID_WIDGET_TYPE.X,
+      'https://x.com/feindura/status/1804519711377436675',
       {
-        type: GRID_WIDGET_TYPE.X_POST,
+        type: GRID_WIDGET_TYPE.X,
         properties: {
-          src: 'https://twitter.com/someuser/status/123456789?ref_src=twsrc%5Etfw',
+          src: 'https://twitter.com/feindura/status/1804519711377436675?ref_src=twsrc%5Etfw',
+          embedType: 'post',
         },
       },
     ],
     [
       'X (Twitter) Post Embed Code',
-      GRID_WIDGET_SUPPORTED_PLATFORMS.X,
-      '<blockquote class="twitter-tweet"><p lang="en" dir="ltr">After many tests and watching the stability of the Ethereum network 🧬, we are finally proposing the Dencun hard fork for the LUKSO Testnet 👈 as a first step towards the Mainnet hard fork, which will follow very soon. ⌚️<br><br>If you are a LUKSO Testnet validator, RPC provider or run… <a href="https://t.co/nByHeouIXB">pic.twitter.com/nByHeouIXB</a></p>&mdash; LUKSO (@lukso_io) <a href="https://twitter.com/lukso_io/status/1830581485331521962?ref_src=twsrc%5Etfw">September 2, 2024</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>',
+      GRID_WIDGET_TYPE.X,
+      '<blockquote class="twitter-tweet"><p lang="en" dir="ltr">Get to know the <a href="https://twitter.com/lukso_io?ref_src=twsrc%5Etfw">@lukso_io</a> ecosystem in 3min 🎧 <a href="https://t.co/UUh23NgdfQ">pic.twitter.com/UUh23NgdfQ</a></p>&mdash; Fabian Vogelsteller (@feindura) <a href="https://twitter.com/feindura/status/1804519711377436675?ref_src=twsrc%5Etfw">June 22, 2024</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>',
       {
-        type: GRID_WIDGET_TYPE.X_POST,
+        type: GRID_WIDGET_TYPE.X,
         properties: {
-          src: 'https://twitter.com/lukso_io/status/1830581485331521962?ref_src=twsrc%5Etfw',
+          src: 'https://twitter.com/feindura/status/1804519711377436675?ref_src=twsrc%5Etfw',
+          embedType: 'post',
         },
       },
     ],
     [
       'X (Twitter) Timeline URL',
-      GRID_WIDGET_SUPPORTED_PLATFORMS.X,
+      GRID_WIDGET_TYPE.X,
       'https://twitter.com/lukso_io',
       {
-        type: GRID_WIDGET_TYPE.X_TIMELINE,
+        type: GRID_WIDGET_TYPE.X,
         properties: {
           src: 'https://twitter.com/lukso_io?ref_src=twsrc%5Etfw',
+          embedType: 'timeline',
         },
       },
     ],
     [
       'X (Twitter) Timeline Embed Code',
-      GRID_WIDGET_SUPPORTED_PLATFORMS.X,
+      GRID_WIDGET_TYPE.X,
       '<a class="twitter-timeline" href="https://twitter.com/lukso_io?ref_src=twsrc%5Etfw">Tweets by lukso_io</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>',
       {
-        type: GRID_WIDGET_TYPE.X_TIMELINE,
+        type: GRID_WIDGET_TYPE.X,
         properties: {
           src: 'https://twitter.com/lukso_io?ref_src=twsrc%5Etfw',
+          embedType: 'timeline',
         },
       },
     ],
     [
       'YouTube URL',
-      GRID_WIDGET_SUPPORTED_PLATFORMS.YOUTUBE,
+      GRID_WIDGET_TYPE.YOUTUBE,
       'https://www.youtube.com/watch?v=Vw4JE64hsO8',
       {
         type: GRID_WIDGET_TYPE.IFRAME,
@@ -69,7 +72,7 @@ describe('Widget Input Parsing', () => {
     ],
     [
       'YouTube Embed Code',
-      GRID_WIDGET_SUPPORTED_PLATFORMS.YOUTUBE,
+      GRID_WIDGET_TYPE.YOUTUBE,
       '<iframe width="560" height="315" src="https://www.youtube.com/embed/Vw4JE64hsO8?si=OwVdFZCStaL90G2W" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
       {
         type: GRID_WIDGET_TYPE.IFRAME,
@@ -81,7 +84,7 @@ describe('Widget Input Parsing', () => {
     ],
     [
       'Spotify Track URL',
-      GRID_WIDGET_SUPPORTED_PLATFORMS.SPOTIFY,
+      GRID_WIDGET_TYPE.SPOTIFY,
       'https://open.spotify.com/track/7xGfFoTpQ2E7fRF5lN10tr',
       {
         type: GRID_WIDGET_TYPE.IFRAME,
@@ -93,7 +96,7 @@ describe('Widget Input Parsing', () => {
     ],
     [
       'Spotify Track Embed Code',
-      GRID_WIDGET_SUPPORTED_PLATFORMS.SPOTIFY,
+      GRID_WIDGET_TYPE.SPOTIFY,
       '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/2BHj31ufdEqVK5CkYDp9mA?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
       {
         type: GRID_WIDGET_TYPE.IFRAME,
@@ -105,7 +108,7 @@ describe('Widget Input Parsing', () => {
     ],
     [
       'Spotify Playlist URL',
-      GRID_WIDGET_SUPPORTED_PLATFORMS.SPOTIFY,
+      GRID_WIDGET_TYPE.SPOTIFY,
       'https://open.spotify.com/playlist/7KFoK4LJ23EncELJwYmTDG',
       {
         type: GRID_WIDGET_TYPE.IFRAME,
@@ -117,7 +120,7 @@ describe('Widget Input Parsing', () => {
     ],
     [
       'Spotify Playlist Embed Code',
-      GRID_WIDGET_SUPPORTED_PLATFORMS.SPOTIFY,
+      GRID_WIDGET_TYPE.SPOTIFY,
       '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/7KFoK4LJ23EncELJwYmTDG?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
       {
         type: GRID_WIDGET_TYPE.IFRAME,
@@ -129,7 +132,7 @@ describe('Widget Input Parsing', () => {
     ],
     [
       'Spotify Artist URL',
-      GRID_WIDGET_SUPPORTED_PLATFORMS.SPOTIFY,
+      GRID_WIDGET_TYPE.SPOTIFY,
       'https://open.spotify.com/artist/4KY9rCrokaoFzvMfX98u1q',
       {
         type: GRID_WIDGET_TYPE.IFRAME,
@@ -141,7 +144,7 @@ describe('Widget Input Parsing', () => {
     ],
     [
       'Spotify Artist Embed Code',
-      GRID_WIDGET_SUPPORTED_PLATFORMS.SPOTIFY,
+      GRID_WIDGET_TYPE.SPOTIFY,
       '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/4KY9rCrokaoFzvMfX98u1q?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
       {
         type: GRID_WIDGET_TYPE.IFRAME,
@@ -153,7 +156,7 @@ describe('Widget Input Parsing', () => {
     ],
     [
       'SoundCloud Track Embed Code',
-      GRID_WIDGET_SUPPORTED_PLATFORMS.SOUNDCLOUD,
+      GRID_WIDGET_TYPE.SOUNDCLOUD,
       '<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1856391039&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/occams-laser" title="Occams Laser" target="_blank" style="color: #cccccc; text-decoration: none;">Occams Laser</a> · <a href="https://soundcloud.com/occams-laser/with-you" title="With You" target="_blank" style="color: #cccccc; text-decoration: none;">With You</a></div>',
       {
         type: GRID_WIDGET_TYPE.IFRAME,
@@ -165,7 +168,7 @@ describe('Widget Input Parsing', () => {
     ],
     [
       'SoundCloud Playlist Embed Code',
-      GRID_WIDGET_SUPPORTED_PLATFORMS.SOUNDCLOUD,
+      GRID_WIDGET_TYPE.SOUNDCLOUD,
       '<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1850298147&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/vaporwavearchive" title="vaporwave archive" target="_blank" style="color: #cccccc; text-decoration: none;">vaporwave archive</a> · <a href="https://soundcloud.com/vaporwavearchive/sets/middle-mgmt-its-easy-to-talk" title="Middle Mgmt - 夏を語るは易し // it&#x27;s easy to talk about summer" target="_blank" style="color: #cccccc; text-decoration: none;">Middle Mgmt - 夏を語るは易し // it&#x27;s easy to talk about summer</a></div>',
       {
         type: GRID_WIDGET_TYPE.IFRAME,
--- domains/grid/utils/gridParser.ts
+++ domains/grid/utils/gridParser.ts
@@ -1,26 +1,26 @@
-import { GRID_WIDGET_SUPPORTED_PLATFORMS } from '../shared/config'
+import { GRID_WIDGET_TYPE } from '../shared/config'
 
-export function parseSupportedPlatformInput(
-  platform: GRID_WIDGET_SUPPORTED_PLATFORMS,
+export const parseSupportedPlatformInput = (
+  platform: GRID_WIDGET_TYPE,
   input: string
-): Partial<GridWidget> | undefined {
+): LayoutItemExtended | undefined => {
   switch (platform) {
-    case GRID_WIDGET_SUPPORTED_PLATFORMS.X:
+    case GRID_WIDGET_TYPE.X:
       return parseXWidgetInput(input)
-    case GRID_WIDGET_SUPPORTED_PLATFORMS.YOUTUBE:
+    case GRID_WIDGET_TYPE.YOUTUBE:
       return parseYoutubeWidgetInput(input)
-    case GRID_WIDGET_SUPPORTED_PLATFORMS.SPOTIFY:
+    case GRID_WIDGET_TYPE.SPOTIFY:
       return parseSpotifyWidgetInput(input)
-    case GRID_WIDGET_SUPPORTED_PLATFORMS.SOUNDCLOUD:
+    case GRID_WIDGET_TYPE.SOUNDCLOUD:
       return parseSoundCloudWidgetInput(input)
     default:
       return
   }
 }
 
-function parseYoutubeWidgetInput(
+const parseYoutubeWidgetInput = (
   input: string
-): Partial<GridWidget> | undefined {
+): LayoutItemExtended | undefined => {
   const YOUTUBE_URL_REGEX =
     /(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?v=([^&]+)/
   const YOUTUBE_EMBED_REGEX =
@@ -29,6 +29,7 @@ function parseYoutubeWidgetInput(
     'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
 
   const youtubeUrlMatch = input.match(YOUTUBE_URL_REGEX)
+
   if (youtubeUrlMatch) {
     return {
       type: GRID_WIDGET_TYPE.IFRAME,
@@ -40,6 +41,7 @@ function parseYoutubeWidgetInput(
   }
 
   const youtubeEmbedMatch = input.match(YOUTUBE_EMBED_REGEX)
+
   if (youtubeEmbedMatch) {
     return {
       type: GRID_WIDGET_TYPE.IFRAME,
@@ -53,40 +55,51 @@ function parseYoutubeWidgetInput(
   return
 }
 
-function parseXWidgetInput(input: string): Partial<GridWidget> | undefined {
-  const X_URL_REGEX =
-    /https?:\/\/(?:www\.)?(?:x\.com|twitter\.com)\/(\w+)(?:\/status\/(\d+))?/
+export const parseXWidgetInput = (
+  input: string
+): LayoutItemExtended | never => {
+  const X_POST_REGEX =
+    /https?:\/\/(?:www\.)?(?:x\.com|twitter\.com)\/(\w+)\/status\/(\d+)(?:\?ref_src=twsrc%5Etfw)?/
+  const X_TIMELINE_REGEX =
+    /https?:\/\/(?:www\.)?(?:x\.com|twitter\.com)\/(\w+)(?:\?ref_src=twsrc%5Etfw)?/
 
-  const xUrlMatch = input.match(X_URL_REGEX)
-  if (xUrlMatch) {
-    if (xUrlMatch[2])
-      return {
-        type: GRID_WIDGET_TYPE.X_POST,
-        properties: {
-          src: `https://twitter.com/${xUrlMatch[1]}/status/${xUrlMatch[2]}?ref_src=twsrc%5Etfw`,
-        },
-      }
+  const xPostMatch = input.match(X_POST_REGEX)
 
+  if (xPostMatch) {
     return {
-      type: GRID_WIDGET_TYPE.X_TIMELINE,
+      type: GRID_WIDGET_TYPE.X,
       properties: {
-        src: `https://twitter.com/${xUrlMatch[1]}?ref_src=twsrc%5Etfw`,
+        src: `https://twitter.com/${xPostMatch[1]}/status/${xPostMatch[2]}?ref_src=twsrc%5Etfw`,
+        embedType: 'post',
       },
     }
   }
 
-  return
+  const xTimelineMatch = input.match(X_TIMELINE_REGEX)
+
+  if (xTimelineMatch) {
+    return {
+      type: GRID_WIDGET_TYPE.X,
+      properties: {
+        src: `https://twitter.com/${xTimelineMatch[1]}?ref_src=twsrc%5Etfw`,
+        embedType: 'timeline',
+      },
+    }
+  }
+
+  throw new Error('Invalid X input')
 }
 
-function parseSpotifyWidgetInput(
+const parseSpotifyWidgetInput = (
   input: string
-): Partial<GridWidget> | undefined {
+): LayoutItemExtended | undefined => {
   const SPOTIFY_URL_REGEX =
     /https?:\/\/(?:open\.)?spotify\.com\/(?:embed\/)?(track|playlist|artist)\/([^?]+)(?:\?utm_source=generator)?/
   const SPOTIFY_IFRAME_ALLOW =
     'autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
 
   const spotifyMatch = input.match(SPOTIFY_URL_REGEX)
+
   if (spotifyMatch) {
     return {
       type: GRID_WIDGET_TYPE.IFRAME,
@@ -100,15 +113,16 @@ function parseSpotifyWidgetInput(
   return
 }
 
-function parseSoundCloudWidgetInput(
+const parseSoundCloudWidgetInput = (
   input: string
-): Partial<GridWidget> | undefined {
+): LayoutItemExtended | undefined => {
   const SOUNDCLOUD_URL_REGEX =
     /https?:\/\/w\.soundcloud\.com\/player\/\?url=https%3A\/\/api\.soundcloud\.com\/(tracks|playlists)\/(\d+)([^"]*)/
   const SOUNDCLOUD_IFRAME_ALLOW =
     'autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
 
   const soundCloudMatch = input.match(SOUNDCLOUD_URL_REGEX)
+
   if (soundCloudMatch) {
     return {
       type: GRID_WIDGET_TYPE.IFRAME,
