3e9f84c266a7b9a02fcd9dafbf5eeaf1dd0931c7
Dominik Zborowski <dominik.zborowski@gmail.com>
2024-10-04 16:10:59 +0200

    Biome fix

--- domains/grid/utils/__tests__/createWidgetObject.spec.ts
+++ domains/grid/utils/__tests__/createWidgetObject.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, vi } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import { createWidgetObject } from '../createWidgetObject'
 
 vi.mock('/domains/grid/utils/generateItemId', () => ({
