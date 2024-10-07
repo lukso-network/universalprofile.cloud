288b8b678f9cdb001434b62155ec2a5cafd533c7
Eduardo Sacco <3680995+nastita@users.noreply.github.com>
2024-10-07 14:23:19 -0300

    Update SoundCloud input parsing

--- domains/grid/utils/__tests__/gridParser.spec.ts
+++ domains/grid/utils/__tests__/gridParser.spec.ts
@@ -1,5 +1,9 @@
 import { describe, expect, it } from 'vitest'
-import { parsePlatformInput } from '../gridParser'
+import {
+  getSoundCloudEmbedUrl,
+  parsePlatformInput,
+  parseSoundCloudWidgetInputFromEmbed,
+} from '../gridParser'
 
 const YOUTUBE_IFRAME_ALLOW =
   'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
@@ -209,13 +213,26 @@ describe('Widget Input Parsing', () => {
       },
     ],
     [
-      'SoundCloud Track Embed Code',
+      'SoundCloud Track Share URL',
       GRID_WIDGET_TYPE.SOUNDCLOUD,
-      '<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1856391039&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/occams-laser" title="Occams Laser" target="_blank" style="color: #cccccc; text-decoration: none;">Occams Laser</a> · <a href="https://soundcloud.com/occams-laser/with-you" title="With You" target="_blank" style="color: #cccccc; text-decoration: none;">With You</a></div>',
+      'https://soundcloud.com/occams-laser/with-you',
       {
         type: GRID_WIDGET_TYPE.IFRAME,
         properties: {
-          src: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1856391039&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+          src: 'https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F1856391039&show_artwork=true',
+          allow: SOUNDCLOUD_IFRAME_ALLOW,
+          embedType: 'tracks',
+        },
+      },
+    ],
+    [
+      'SoundCloud Track Embed Code',
+      GRID_WIDGET_TYPE.SOUNDCLOUD,
+      '<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https://api.soundcloud.com/tracks/1856391039&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/occams-laser" title="Occams Laser" target="_blank" style="color: #cccccc; text-decoration: none;">Occams Laser</a> · <a href="https://soundcloud.com/occams-laser/with-you" title="With You" target="_blank" style="color: #cccccc; text-decoration: none;">With You</a></div>',
+      {
+        type: GRID_WIDGET_TYPE.IFRAME,
+        properties: {
+          src: 'https://w.soundcloud.com/player/?url=https://api.soundcloud.com/tracks/1856391039&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
           allow: SOUNDCLOUD_IFRAME_ALLOW,
           embedType: 'tracks',
         },
@@ -224,18 +241,62 @@ describe('Widget Input Parsing', () => {
     [
       'SoundCloud Playlist Embed Code',
       GRID_WIDGET_TYPE.SOUNDCLOUD,
-      '<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1850298147&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/vaporwavearchive" title="vaporwave archive" target="_blank" style="color: #cccccc; text-decoration: none;">vaporwave archive</a> · <a href="https://soundcloud.com/vaporwavearchive/sets/middle-mgmt-its-easy-to-talk" title="Middle Mgmt - 夏を語るは易し // it&#x27;s easy to talk about summer" target="_blank" style="color: #cccccc; text-decoration: none;">Middle Mgmt - 夏を語るは易し // it&#x27;s easy to talk about summer</a></div>',
+      '<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https://api.soundcloud.com/playlists/1850298147&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/vaporwavearchive" title="vaporwave archive" target="_blank" style="color: #cccccc; text-decoration: none;">vaporwave archive</a> · <a href="https://soundcloud.com/vaporwavearchive/sets/middle-mgmt-its-easy-to-talk" title="Middle Mgmt - 夏を語るは易し // it&#x27;s easy to talk about summer" target="_blank" style="color: #cccccc; text-decoration: none;">Middle Mgmt - 夏を語るは易し // it&#x27;s easy to talk about summer</a></div>',
       {
         type: GRID_WIDGET_TYPE.IFRAME,
         properties: {
-          src: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1850298147&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+          src: 'https://w.soundcloud.com/player/?url=https://api.soundcloud.com/playlists/1850298147&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
           allow: SOUNDCLOUD_IFRAME_ALLOW,
           embedType: 'playlists',
         },
       },
     ],
-  ])('correctly parses %s', (_description, platform, input, expected) => {
-    const result = parsePlatformInput(platform, input)
+  ])('correctly parses %s', async (_description, platform, input, expected) => {
+    const result = await parsePlatformInput(platform, input)
     expect(result).toEqual(expected)
+  })
+})
+
+describe('Widget input parsing helpers', () => {
+  describe('getSoundCloudEmbedUrl', () => {
+    it('gets the SoundCloud embed code from a SoundCloud share URL', async () => {
+      const input = 'https://soundcloud.com/occams-laser/with-you'
+      const result = await getSoundCloudEmbedUrl(input)
+
+      expect(result).toEqual(
+        'https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F1856391039&show_artwork=true'
+      )
+    })
+  })
+
+  describe('parseSoundCloudWidgetInputFromEmbed', () => {
+    it('parses SoundCloud track embed code', () => {
+      const input =
+        '<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https://api.soundcloud.com/tracks/1856391039&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/occams-laser" title="Occams Laser" target="_blank" style="color: #cccccc; text-decoration: none;">Occams Laser</a> · <a href="https://soundcloud.com/occams-laser/with-you" title="With You" target="_blank" style="color: #cccccc; text-decoration: none;">With You</a></div>'
+      const result = parseSoundCloudWidgetInputFromEmbed(input)
+      expect(result).toEqual({
+        type: GRID_WIDGET_TYPE.IFRAME,
+        properties: {
+          src: 'https://w.soundcloud.com/player/?url=https://api.soundcloud.com/tracks/1856391039&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+          allow: SOUNDCLOUD_IFRAME_ALLOW,
+          embedType: 'tracks',
+        },
+      })
+    })
+
+    it('parses SoundCloud track embed code with url encoded characters', () => {
+      const input =
+        '<iframe width="100%" height="400" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F1856391039&show_artwork=true"></iframe>'
+      const result = parseSoundCloudWidgetInputFromEmbed(input)
+
+      expect(result).toEqual({
+        type: GRID_WIDGET_TYPE.IFRAME,
+        properties: {
+          src: 'https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F1856391039&show_artwork=true',
+          allow: SOUNDCLOUD_IFRAME_ALLOW,
+          embedType: 'tracks',
+        },
+      })
+    })
   })
 })
