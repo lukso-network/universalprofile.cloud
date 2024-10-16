c9ba7c48ce2fbddf5b67e7e412801b8b5b7f44e3
Dominik Zborowski <dominik.zborowski@gmail.com>
2024-10-16 15:45:38 +0200

    Reload app on network change

--- components/ModalTemplateSwitchApplicationNetwork.vue
+++ components/ModalTemplateSwitchApplicationNetwork.vue
@@ -3,16 +3,17 @@ const { closeModal, modal } = useModal()
 const { formatMessage } = useIntl()
 
 const handleChangeNetwork = async () => {
-  const { selectedChainId } = storeToRefs(useAppStore())
   const { getNetworkByChainId } = useAppStore()
   const { disconnect } = useBaseProvider()
 
-  selectedChainId.value = modal?.data?.chainId
   await navigateTo({
     path: homeRoute(),
     query: { network: getNetworkByChainId(modal?.data?.chainId).id },
   })
   disconnect()
+
+  // we reload the page to reinitialize web3 instances
+  location.reload()
 }
 </script>
 
