22f6d25507ee10494f8f8f2c3dfdfe6840a9033e
Eduardo Sacco <3680995+nastita@users.noreply.github.com>
2024-10-17 15:24:27 -0300

    wip: refactor grid parser

--- domains/grid/utils/gridParser.ts
+++ domains/grid/utils/gridParser.ts
@@ -1,20 +1,137 @@
+/**
+ * Represents the parameters required for parsing platform-specific input.
+ *
+ * @typedef {Object} PlatformParsingParameters
+ * @property {GridWidgetType} type - The type of the grid widget.
+ * @property {RegExp} embedRegex - The regular expression used to match the embed URL. Named capture groups are used to extract properties.
+ * @property {RegExp[]} [secondaryRegexes] - Optional secondary regular expressions for additional matching. Whole match is used as input for the embed API callback.
+ * @property {(url: string) => Promise<string | undefined>} [embedApiCallback] - Optional callback function to fetch the embed from the platform's API.
+ * @property {Record<string, string>} [constantProperties] - Optional constant properties to be included in the parsed result.
+ */
+export type PlatformParsingParameters = {
+  type: GridWidgetType
+  embedRegex: RegExp
+  secondaryRegexes?: RegExp[]
+  secondaryRegexCallback?: (url: string) => Promise<string | undefined>
+  constantProperties?: Record<string, string>
+}
+
+const PLATFORM_PARSING_PARAMETERS: Record<
+  GridWidgetType,
+  PlatformParsingParameters | undefined
+> = {
+  [GRID_WIDGET_TYPE.TITLE_LINK]: undefined,
+  [GRID_WIDGET_TYPE.TEXT]: undefined,
+  [GRID_WIDGET_TYPE.IFRAME]: undefined,
+  [GRID_WIDGET_TYPE.IMAGE]: undefined,
+  [GRID_WIDGET_TYPE.X]: {
+    type: GRID_WIDGET_TYPE.X,
+    embedRegex:
+      /(?:https?:\/\/)?(?:www\.)?(?:x\.com|twitter\.com)\/(\w+)\/status\/(\d+)(?:\?ref_src=twsrc%5Etfw)?/,
+    // propertyNames: ['src', 'type'],
+  },
+  [GRID_WIDGET_TYPE.INSTAGRAM]: {
+    type: GRID_WIDGET_TYPE.INSTAGRAM,
+    embedRegex:
+      /https:\/\/www\.instagram\.com\/(p|reel|profile|tv)\/([\w-]+)\/(\?[^"]*)?/,
+    // propertyNames: ['src', 'type'],
+  },
+  [GRID_WIDGET_TYPE.WARPCAST]: undefined,
+  [GRID_WIDGET_TYPE.SPOTIFY]: {
+    type: GRID_WIDGET_TYPE.IFRAME,
+    embedRegex:
+      /https?:\/\/(?:open\.)?spotify\.com\/embed\/?(?<type>track|playlist|artist)\/(?<id>[^?]+)(?:\?utm_source=(?:generator|oembed))?(?:&theme=(?<theme>\d))?/,
+    secondaryRegexes: [
+      /https:\/\/open\.spotify\.com\/(?<type>track|playlist|artist)\/(?<id>[^?]+)/,
+    ],
+    secondaryRegexCallback: getSpotifyEmbedUrl,
+    constantProperties: {
+      allow:
+        'autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture',
+    },
+  },
+  [GRID_WIDGET_TYPE.SOUNDCLOUD]: {
+    type: GRID_WIDGET_TYPE.IFRAME,
+    embedRegex:
+      /https?:\/\/w\.soundcloud\.com\/player\/\?(?:(?!url=https).)*url=https(?::|%3A)(?:\/|%2F){2}api\.soundcloud\.com(?:\/|%2F)(?<type>tracks|playlists|users)(?:\/|%2F)\d+(?:[^"]*)?/,
+    secondaryRegexes: [
+      /https:\/\/soundcloud\.com\/([a-zA-Z0-9_-]+)(?:\/(sets\/[a-zA-Z0-9_-]+|[a-zA-Z0-9_-]+))?\/?/,
+    ],
+    secondaryRegexCallback: getSoundCloudEmbedUrl,
+    constantProperties: {
+      allow:
+        'autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture',
+    },
+  },
+  [GRID_WIDGET_TYPE.YOUTUBE]: {
+    type: GRID_WIDGET_TYPE.IFRAME,
+    embedRegex: /(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?v=([^&]+)/,
+  },
+  [GRID_WIDGET_TYPE.ADD_CONTENT]: undefined,
+}
+
 export const parsePlatformInput = async (
   platform: GridWidgetType,
   input: string
 ): Promise<LayoutItemExtended | never> => {
-  switch (platform) {
-    case GRID_WIDGET_TYPE.X:
-      return parseXWidgetInput(input)
-    case GRID_WIDGET_TYPE.INSTAGRAM:
-      return parseInstagramWidgetInput(input)
-    case GRID_WIDGET_TYPE.YOUTUBE:
-      return parseYoutubeWidgetInput(input)
-    case GRID_WIDGET_TYPE.SPOTIFY:
-      return parseSpotifyWidgetInput(input)
-    case GRID_WIDGET_TYPE.SOUNDCLOUD:
-      return await parseSoundCloudWidgetInput(input)
-    default:
-      throw new Error('Invalid platform')
+  const platformParsingParameters = PLATFORM_PARSING_PARAMETERS[platform]
+  if (!platformParsingParameters) throw new Error('Invalid platform')
+
+  // Check if the input matches the embed regex
+  try {
+    return parsePlatformEmbed(input, platformParsingParameters)
+  } catch {}
+
+  const { secondaryRegexes, secondaryRegexCallback } = platformParsingParameters
+  if (!secondaryRegexes || !secondaryRegexCallback)
+    throw new Error('Invalid platform')
+
+  // Check if the input matches a secondary regex
+  let secondaryMatch: RegExpMatchArray | null = null
+  secondaryRegexes.find(regex => {
+    secondaryMatch = input.match(regex)
+
+    return secondaryMatch
+  })
+
+  if (!secondaryMatch) throw new Error('Invalid input')
+
+  const embedApiResult = await secondaryRegexCallback(secondaryMatch[0])
+  if (!embedApiResult) throw new Error('Invalid input')
+
+  return parsePlatformEmbed(embedApiResult, platformParsingParameters)
+}
+
+const parsePlatformEmbed = (
+  input: string,
+  platformParsingParameters: PlatformParsingParameters
+): LayoutItemExtended | never => {
+  const { type, embedRegex, constantProperties } = platformParsingParameters
+  const match = input.match(embedRegex)
+
+  if (!match) throw new Error('Invalid input')
+
+  const { groups } = match
+  let extractedProperties: Record<string, string> = {}
+  if (groups) {
+    // Extract the  properties from capture groups from the regex match
+    extractedProperties = Object.entries(groups).reduce(
+      (acc: Record<string, string>, [key, value]) => {
+        if (value) acc[key] = value
+
+        return acc
+      },
+      {}
+    )
+  }
+
+  return {
+    type,
+    properties: {
+      src: match[0],
+      ...constantProperties,
+      ...extractedProperties,
+    },
   }
 }
 
@@ -111,69 +228,21 @@ const parseSpotifyWidgetInput = (input: string): LayoutItemExtended | never => {
   throw new Error('Invalid Spotify input')
 }
 
-const parseSoundCloudWidgetInput = async (
-  input: string
-): Promise<LayoutItemExtended> => {
-  const SOUNDCLOUD_SHARE_URL_REGEX =
-    /https:\/\/soundcloud\.com\/([a-zA-Z0-9_-]+)(?:\/(sets\/[a-zA-Z0-9_-]+|[a-zA-Z0-9_-]+))?\/?/
-
-  // Check if the input already contains an embed URL
-  try {
-    return parseSoundCloudWidgetInputFromEmbed(input)
-  } catch {}
-
-  // Check if the input is a SoundCloud share URL
-  const [shareUrlMatch] = input.match(SOUNDCLOUD_SHARE_URL_REGEX) || []
-  if (shareUrlMatch) {
-    const soundCloudEmbed = await getSoundCloudEmbedUrl(shareUrlMatch)
-
-    if (soundCloudEmbed) {
-      return parseSoundCloudWidgetInputFromEmbed(soundCloudEmbed)
-    }
-  }
-
-  throw new Error('Invalid SoundCloud input')
-}
-
-const parseSoundCloudWidgetInputFromEmbed = (
-  input: string
-): LayoutItemExtended | never => {
-  const SOUNDCLOUD_EMBED_URL_REGEX =
-    /https?:\/\/w\.soundcloud\.com\/player\/\?(?:(?!url=https).)*url=https(?::|%3A)(?:\/|%2F){2}api\.soundcloud\.com(?:\/|%2F)(tracks|playlists|users)(?:\/|%2F)\d+(?:[^"]*)?/
-  const SOUNDCLOUD_IFRAME_ALLOW =
-    'autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
-  const [match, type] = input.match(SOUNDCLOUD_EMBED_URL_REGEX) || []
-
-  if (match) {
-    return {
-      type: GRID_WIDGET_TYPE.IFRAME,
-      properties: {
-        src: match,
-        allow: SOUNDCLOUD_IFRAME_ALLOW,
-        type,
-      },
-    }
-  }
-
-  throw new Error('Invalid SoundCloud input')
-}
-
-const getSoundCloudEmbedUrl = async (
-  url: string
-): Promise<string | undefined> => {
+async function getSoundCloudEmbedUrl(url: string): Promise<string | undefined> {
   const encodedUrl = encodeURI(url)
   const response = await fetch(
     `https://soundcloud.com/oembed?url=${encodedUrl}&format=json`
   )
 
-  if (!response.ok) {
-    return response.statusText
-  }
+  return response.ok ? ((await response.json())?.html as string) : undefined
+}
 
-  const { html } = await response.json()
-  const [, srcMatch] = html.match(/src="([^"]+)"/) || []
+async function getSpotifyEmbedUrl(url: string): Promise<string | undefined> {
+  const response = await fetch(
+    `https://open.spotify.com/oembed?url=${url}&format=json`
+  )
 
-  return srcMatch
+  return response.ok ? ((await response.json())?.html as string) : undefined
 }
 
 const parseInstagramWidgetInput = (
