219f9c12eae6a38933c00676ce64096679ca7ff7
Dominik Zborowski <dominik.zborowski@gmail.com>
2024-10-18 16:55:54 +0200

    Change inner widget rounding

--- domains/grid/components/GridWidgetImage.vue
+++ domains/grid/components/GridWidgetImage.vue
@@ -7,7 +7,7 @@ defineProps<Props>()
 </script>
 
 <template>
-  <div class="m-3 flex h-full overflow-hidden rounded-12">
+  <div class="m-3 flex h-full overflow-hidden rounded-8">
     <img
       class="min-h-full min-w-full shrink-0 object-cover"
       :src="src"
