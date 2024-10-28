e0def6a82f68ca8e78ae726ef9919857acb086f1
Dominik Zborowski <dominik.zborowski@gmail.com>
2024-10-28 11:52:25 +0100

    Add settings to navbar when not logged in

--- components/AppNavbarLanding.vue
+++ components/AppNavbarLanding.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-const { isTestnet, isConnected } = storeToRefs(useAppStore())
+const { isTestnet, isConnected, connectedProfileAddress } =
+  storeToRefs(useAppStore())
 const { disconnect } = useBaseProvider()
 const { showModal } = useModal()
 
@@ -13,6 +14,18 @@ const handleConnect = async () => {
 const handleDisconnect = async () => {
   disconnect()
 }
+
+const handleNavigateSettings = () => {
+  navigateTo(settingsRoute())
+}
+
+const handleNavigateProfile = async () => {
+  navigateTo(profileRoute(connectedProfileAddress.value))
+}
+
+const handleNavigateMyUpDashboard = () => {
+  window.open(myUpDappDashboardUrl(), '_self')
+}
 </script>
 
 <template>
@@ -21,8 +34,14 @@ const handleDisconnect = async () => {
     logo-url="/images/logo-ue.svg"
     is-transparent
     has-menu
+    mobile-breakpoint="lg"
   >
-    <div class="flex w-full items-center justify-end gap-4" slot="desktop-menu">
+    <!-- Desktop Menu -->
+    <div
+      class="flex w-full items-center justify-end gap-4 pr-4"
+      slot="desktop-menu"
+    >
+      <!-- L14 Profiles -->
       <lukso-button
         variant="text"
         is-link
@@ -41,6 +60,8 @@ const handleDisconnect = async () => {
           color="purple-51"
         ></lukso-icon>
       </lukso-button>
+
+      <!-- Support -->
       <lukso-button
         variant="text"
         is-link
@@ -53,6 +74,8 @@ const handleDisconnect = async () => {
           {{ $formatMessage('footer_need_help_text') }}
         </span>
       </lukso-button>
+
+      <!-- Create Profile -->
       <lukso-button
         variant="secondary"
         is-link
@@ -70,7 +93,11 @@ const handleDisconnect = async () => {
           }}
         </span>
       </lukso-button>
+
+      <!-- Profile dropdown -->
       <AppNavbarProfileDropdown v-if="isConnected" />
+
+      <!-- Connect -->
       <lukso-button
         v-else
         variant="landing"
@@ -80,9 +107,25 @@ const handleDisconnect = async () => {
       >
         {{ $formatMessage('header_connect') }}
       </lukso-button>
+
+      <!-- Settings -->
+      <div
+        v-if="!isConnected"
+        class="h-6 w-[1px] border-l border-purple-82 pl-4"
+      >
+        <lukso-icon
+          name="settings"
+          color="purple-41"
+          class="cursor-pointer opacity-70 transition hover:rotate-45 hover:opacity-100"
+          @click="handleNavigateSettings"
+        ></lukso-icon>
+      </div>
     </div>
+
+    <!-- Mobile Menu -->
     <div slot="mobile-menu">
       <div className="flex flex-col items-center justify-center h-screen pb-32">
+        <!-- L14 Profiles -->
         <lukso-button
           variant="text"
           custom-class="no-underline"
@@ -109,6 +152,8 @@ const handleDisconnect = async () => {
             color="purple-41"
           ></lukso-icon>
         </lukso-button>
+
+        <!-- Support -->
         <lukso-button
           variant="text"
           is-link
@@ -123,6 +168,8 @@ const handleDisconnect = async () => {
             {{ $formatMessage('footer_need_help_text') }}
           </span>
         </lukso-button>
+
+        <!-- Create Profile -->
         <lukso-button
           variant="text"
           custom-class="no-underline"
@@ -153,6 +200,58 @@ const handleDisconnect = async () => {
             color="purple-41"
           ></lukso-icon>
         </lukso-button>
+
+        <!-- Profile -->
+        <lukso-button
+          v-if="isConnected"
+          variant="text"
+          custom-class="text-12 nav-apax-12-medium-uppercase"
+          class="group"
+          @click="handleNavigateProfile"
+        >
+          <span class="text-purple-63 transition group-hover:text-purple-41">
+            {{ $formatMessage('header_my_profile') }}
+          </span>
+        </lukso-button>
+
+        <!-- Dashboard (my.up.cloud) -->
+        <lukso-button
+          v-if="isConnected"
+          variant="text"
+          custom-class="text-12 nav-apax-12-medium-uppercase"
+          class="group"
+          @click="handleNavigateMyUpDashboard"
+        >
+          <span class="text-purple-63 transition group-hover:text-purple-41">
+            {{ $formatMessage('header_relayer_dashboard') }}
+          </span>
+          <lukso-icon
+            name="link-3"
+            size="small"
+            class="ml-2 group-hover:hidden"
+            color="purple-63"
+          ></lukso-icon>
+          <lukso-icon
+            name="link-3"
+            size="small"
+            class="ml-2 hidden group-hover:inline-block"
+            color="purple-41"
+          ></lukso-icon>
+        </lukso-button>
+
+        <!-- Settings -->
+        <lukso-button
+          variant="text"
+          custom-class="text-12 nav-apax-12-medium-uppercase"
+          class="group"
+          @click="handleNavigateSettings"
+        >
+          <span class="text-purple-63 transition group-hover:text-purple-41">
+            {{ $formatMessage('header_settings') }}
+          </span>
+        </lukso-button>
+
+        <!-- Disconnect -->
         <lukso-button
           v-if="isConnected"
           variant="text"
@@ -164,6 +263,8 @@ const handleDisconnect = async () => {
             {{ $formatMessage('header_disconnect') }}
           </span>
         </lukso-button>
+
+        <!-- Connect -->
         <lukso-button
           v-else
           variant="text"
