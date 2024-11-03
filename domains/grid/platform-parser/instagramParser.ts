8c82101057df57ea47cb60c5f38e45571e4ed483
Eduardo Sacco <3680995+nastita@users.noreply.github.com>
2024-11-03 11:54:43 -0300

    Update IG input parsing

--- domains/grid/platform-parser/instagramParser.ts
+++ domains/grid/platform-parser/instagramParser.ts
@@ -5,7 +5,15 @@ export const PLATFORM_PARSING_PARAMETERS_INSTAGRAM: PlatformParsingParameters =
       {
         regex:
           /https:\/\/www\.instagram\.com\/(?<type>p|reel|profile|tv)\/(?<id>[\w-]+)\/(?<params>\?[^"]*)?/,
-        callback: async () => undefined,
+        callback: async (matches: RegExpMatchArray[]) => {
+          const properties = getPropertiesFromGroups(matches)
+          const { type, id } = properties
+
+          return {
+            type,
+            id,
+          }
+        },
       },
     ],
   }
