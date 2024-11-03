771b7ba590985f09d34506a7237f8bbb691a1fbb
Dominik Zborowski <dominik.zborowski@gmail.com>
2024-11-04 00:31:32 +0100

    Fix schema

--- domains/schema/xWidgetSchema.ts
+++ domains/schema/xWidgetSchema.ts
@@ -3,8 +3,8 @@ import { z } from 'zod'
 export const xWidgetSchema = z
   .object({
     src: z.string(),
-    type: z.string(),
-    username: z.string(),
+    type: z.string().optional(),
+    username: z.string().optional(),
     id: z.string().optional(),
     theme: z.string().optional(),
     language: z.string().optional(),
