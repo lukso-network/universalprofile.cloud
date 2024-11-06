8e88116412c075570f74e5712d711bc89ca37a39
Dominik Zborowski <dominik.zborowski@gmail.com>
2024-11-07 00:10:54 +0100

    Reload iframe on resize

--- domains/grid/components/GridWidgetIframe.vue
+++ domains/grid/components/GridWidgetIframe.vue
@@ -1,24 +1,49 @@
 <script setup lang="ts">
+import { useDebounceFn, useResizeObserver } from '@vueuse/core'
+
 type Props = {
   src: string
   allow?: string
   referrerpolicy?: IframeReferrerPolicy
   allowfullscreen?: boolean
+  widget?: GridWidget
 }
 
-defineProps<Props>()
+const props = defineProps<Props>()
+const embedSrc = ref(props.src)
+const iframeRef = ref<HTMLElement | null>(null)
+const isLoaded = ref(false)
+
+const handleLoad = () => {
+  isLoaded.value = true
+}
+
+const reloadIframe = () => {
+  embedSrc.value = ''
+  const onResize = useDebounceFn(() => {
+    embedSrc.value = props.src
+  }, 100)
+  onResize()
+}
+
+useResizeObserver(iframeRef, useDebounceFn(reloadIframe, 100))
 </script>
 
 <template>
-  <div class="h-full p-3">
+  <div ref="iframeRef" class="relative h-full p-3">
+    <AppLoader
+      v-if="!isLoaded"
+      class="absolute left-[calc(50%-20px)] top-[calc(50%-20px)]"
+    />
     <iframe
-      :src="src"
+      :src="embedSrc"
       :allow="allow"
       :referrerpolicy="referrerpolicy"
       :allowfullscreen="allowfullscreen"
       frameborder="0"
-      loading="lazy"
       class="size-full overflow-hidden rounded-8"
-    ></iframe>
+      @load="handleLoad"
+    >
+    </iframe>
   </div>
 </template>
