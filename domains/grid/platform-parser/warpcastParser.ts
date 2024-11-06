dc2971be4e4bec6fd19c4ccfb11b682023dd3f6d
Eduardo Sacco <3680995+nastita@users.noreply.github.com>
2024-11-06 02:13:26 -0300

    feat: warpcast input parser updates (#530)

--- domains/grid/platform-parser/warpcastParser.ts
+++ domains/grid/platform-parser/warpcastParser.ts
@@ -3,11 +3,12 @@ export const PLATFORM_PARSING_PARAMETERS_WARPCAST: PlatformParsingParameters = {
     // Match whole embed code
     {
       regex: createIframeRegex(
-        'https:\\/\\/warpcast\\.com\\/(?<username>[\\w.-]+)'
+        '@?(?:https:\\/\\/warpcast\\.com\\/)?(?<username>[\\w.-]+)(?:\\/(?<id>[\\w]+))?'
       ),
       callback: async (matches: RegExpMatchArray[]) => {
         return processIframeAttributes(matches, {
-          createSrc: ({ username }) => `https://warpcast.com/${username}`,
+          createSrc: ({ username, id }) =>
+            `https://warpcast.com/${username}${id ? `/${id}` : ''}`,
           defaultAllow:
             'clipboard-write; encrypted-media; fullscreen; picture-in-picture',
           additionalProcessing: properties => ({
